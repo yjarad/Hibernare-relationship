ed150d3fb3f0b53cf915a2c93c98e356313855c4
660093 <660093@PCIN490601.cts.com>
2020-02-26 19:24:02 +0530

    change working

--- src/main/java/com/edu/yog/hibernate/releationship/HibernateReleationshipApplication.java
+++ src/main/java/com/edu/yog/hibernate/releationship/HibernateReleationshipApplication.java
@@ -2,23 +2,25 @@ package com.edu.yog.hibernate.releationship;
 
 import javax.persistence.EntityManager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import com.edu.yog.hibernate.releationship.model.Comment;
 import com.edu.yog.hibernate.releationship.model.Post;
 
+
 @SpringBootApplication
 public class HibernateReleationshipApplication {
+	@Autowired
+	private static LocalContainerEntityManagerFactoryBean bean;
+
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(HibernateReleationshipApplication.class, args);
+
 		LocalContainerEntityManagerFactoryBean factoryBean = context
 				.getBean(LocalContainerEntityManagerFactoryBean.class);
 		PlatformTransactionManager manager = context.getBean(PlatformTransactionManager.class);
@@ -34,6 +36,7 @@ public class HibernateReleationshipApplication {
 		entityManager.flush();
 		entityManager.getTransaction().commit();
 		System.out.println("Record persited succeesfully ");
+
 	}
 
 }
--- src/main/java/com/edu/yog/hibernate/releationship/dao/PostService.java
+++ src/main/java/com/edu/yog/hibernate/releationship/dao/PostService.java
@@ -1,6 +1,30 @@
 package com.edu.yog.hibernate.releationship.dao;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.stereotype.Component;
+
+import com.edu.yog.hibernate.releationship.model.Comment;
+import com.edu.yog.hibernate.releationship.model.Post;
+
+@Component
 public class PostService {
+	
 	public PostService() {
+//		System.out.println("factoryBean" + bean);
+//		EntityManager entityManager = bean.getNativeEntityManagerFactory().createEntityManager();
+//		System.out.println("factoryBean" + entityManager);
+//		entityManager.getTransaction().begin();
+//		Post post = new Post("First post");
+//		post.addComment(new Comment("My first review"));
+//		post.addComment(new Comment("My second review"));
+//		post.addComment(new Comment("My third review"));
+//		entityManager.persist(post);
+//		entityManager.flush();
+//		entityManager.getTransaction().commit();
+//		System.out.println("Record persited succeesfully ");
 	}
 }
